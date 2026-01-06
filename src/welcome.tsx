5b924e6c7a5305f5af005c42380ffee199593ce5
DuartePortfolio <40230362@esmad.ipp.pt>
2026-01-06 17:22:15 +0000

    initial commit

--- src/welcome.tsx
+++ src/welcome.tsx
@@ -1,45 +1,35 @@
 import type { JSX } from "react/jsx-dev-runtime";
-import image from "./assets/img.svg";
 import "./css/welcome.css";
-import vector1 from "./assets/vector 1.svg";
-import vector2 from "./assets/vector 2.svg";
-import vector3 from "./assets/vector 3.svg";
-import vector4 from "./assets/vector 4.svg";
-import vector5 from "./assets/vector 5.svg";
+import vector1 from "./assets/Vector 1.svg";
+import vector2 from "./assets/Vector 2.svg";
+import vector3 from "./assets/Vector 3.svg";
+import vector4 from "./assets/Vector 4.svg";
+import vector5 from "./assets/Vector 5.svg";
 import vector from "./assets/vector.svg";
 import rocket from "./assets/rocket-2-svgrepo-com 1.svg";
 
 export const Welcome = (): JSX.Element => {
   return (
-    <div className="welcome">
-      <div className="blobs">
-        <img className="vector" alt="Vector" src={vector1} />
-
-        <img className="vector-6" alt="Vector" src={vector} />
-
-        <img className="vector-2" alt="Vector" src={vector3} />
-
-        <img className="vector-3" alt="Vector" src={vector4} />
-
-        <img className="vector-4" alt="Vector" src={vector2} />
-
-        <img className="vector-5" alt="Vector" src={vector5} />
+    <div className="welcome-container">
+      <div className="background-blobs">
+        <img className="blob-bottom-left" alt="Vector" src={vector1} />
+        <img className="blob-bottom-center" alt="Vector" src={vector} />
+        <img className="blob-bottom-right" alt="Vector" src={vector3} />
+        <img className="blob-top-left" alt="Vector" src={vector4} />
+        <img className="blob-top-right" alt="Vector" src={vector2} />
+        <img className="blob-middle-right" alt="Vector" src={vector5} />
       </div>
 
-      <div className="rectangle" />
+      <div className="progress-bar-bg" />
+      <div className="progress-bar-fill" />
 
-      <div className="div" />
+      <div className="welcome-text">Welcome to</div>
+      <div className="app-title">Voltzy</div>
 
-      <div className="text-wrapper">Welcome to</div>
-
-      <div className="text-wrapper-2">Voltzy</div>
-
-      <img className="rocket" alt="Vector" src={rocket} />
-
-      <img className="img" alt="Vector" src={image} />
-
-      <div className="text-wrapper-3">Get started</div>
-
+      <button className="get-started-button">
+        <img className="rocket-icon" alt="rocket" src={rocket} />
+        <span className="button-text">Get started</span>
+      </button>
     </div>
   );
 };
